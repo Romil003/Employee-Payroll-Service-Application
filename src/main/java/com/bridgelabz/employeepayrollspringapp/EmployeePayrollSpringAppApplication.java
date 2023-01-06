87574b7f52f1db8230fa9fc540923fbd02aa3882
Romil003 <romilghadge33@gmail.com>
2023-01-06 18:17:51 +0530

    Added : Database setting as environment user

--- src/main/java/com/bridgelabz/employeepayrollspringapp/EmployeePayrollSpringAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollspringapp/EmployeePayrollSpringAppApplication.java
@@ -14,6 +14,8 @@ public class EmployeePayrollSpringAppApplication {
         ApplicationContext context = SpringApplication.run(EmployeePayrollSpringAppApplication.class, args);
         log.info("Employee Payroll App Started in {} Environment",
                 context.getEnvironment().getProperty("environment"));
+        log.info("Employee Payroll DB User is {}",
+                context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
