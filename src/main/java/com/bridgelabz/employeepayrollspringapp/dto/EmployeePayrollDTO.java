1acc7c62e4c4de2ce531e4e04bf79b485e616961
Romil003 <romilghadge33@gmail.com>
2023-01-09 14:16:28 +0530

    Added : adding validations for all variables and exception handler for startDate

--- src/main/java/com/bridgelabz/employeepayrollspringapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollspringapp/dto/EmployeePayrollDTO.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.employeepayrollspringapp.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import lombok.NonNull;
 import lombok.ToString;
 
 import java.time.LocalDate;
@@ -18,13 +18,20 @@ public class EmployeePayrollDTO {
     @Min(value = 500,message = "Minimum Wage should be more thn 500")
     public long salary;
 
+    @Pattern(regexp = "male|female",message = "Gender needs to be male or female")
     public String gender;
 
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "startDate should not be empty")
+    @PastOrPresent(message = "startDate should be past or today's date")
     public LocalDate startDate;
 
+    @NotBlank(message = "Note cannot be empty")
     public String note;
 
+    @NotBlank(message = "profilePic cannot be empty")
     public String profilePic;
 
+    @NotNull(message = "department should not be empty")
     public List<String> departments;
 }
